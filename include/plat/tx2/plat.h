85dbb9d721c71afd7ea802a57372fd2e24eb9553
Sebastian Eckl <sebastian.eckl@hensoldt.net>
2023-01-11 20:08:34 +0100

    [DEV] add support for Jetson TX2 and Xavier

---
+++ include/plat/tx2/plat.h
@@ -0,0 +1,10 @@
+/*
+ * Basic plat.h (to be extended in the future)
+ *
+ * Copyright (C) 2022, HENSOLDT Cyber GmbH
+ */
+
+#define TX_CHANNEL_PADDR 0x3004e000
+#define TX_CHANNEL_SIZE 0x1000
+#define RX_CHANNEL_PADDR 0x3004f000
+#define RX_CHANNEL_SIZE 0x1000
---
+++ include/plat/xavier/plat.h
@@ -0,0 +1,10 @@
+/*
+ * Basic plat.h (to be extended in the future)
+ *
+ * Copyright (C) 2022, HENSOLDT Cyber GmbH
+ */
+
+#define TX_CHANNEL_PADDR 0x4004e000
+#define TX_CHANNEL_SIZE 0x1000
+#define RX_CHANNEL_PADDR 0x4004f000
+#define RX_CHANNEL_SIZE 0x1000
