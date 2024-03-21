20a9382a288baa49c54770722a56ce59c812b580
Felix Schladt <felix.schladt@hensoldt.net>
2024-03-21 11:59:45 +0100

    [DOC] Update Copyright
    Change files to GPLv2 or Commercial licensing for HC files

--- include/plat/tx2/plat.h
+++ include/plat/tx2/plat.h
@@ -1,9 +1,12 @@
 /*
  * Basic plat.h (to be extended in the future)
  *
- * Copyright (C) 2022, HENSOLDT Cyber GmbH
+ * Copyright (C) 2022-2024, HENSOLDT Cyber GmbH
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * For commercial licensing, contact: info.cyber@hensoldt.net
  */
-
 #define TX_CHANNEL_PADDR 0x3004e000
 #define TX_CHANNEL_SIZE 0x1000
 #define RX_CHANNEL_PADDR 0x3004f000
--- include/plat/xavier/plat.h
+++ include/plat/xavier/plat.h
@@ -1,7 +1,11 @@
 /*
  * Basic plat.h (to be extended in the future)
+ * 
+ * Copyright (C) 2022-2024, HENSOLDT Cyber GmbH
  *
- * Copyright (C) 2022, HENSOLDT Cyber GmbH
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * For commercial licensing, contact: info.cyber@hensoldt.net
  */
 
 #define TX_CHANNEL_PADDR 0x4004e000
